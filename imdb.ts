338671a63dfb787afa4eed963ff94e66db5f7c86
picatostao <valver123@gmail.com>
2023-05-16 12:29:05 +0200

    commit

--- imdb.ts
+++ imdb.ts
@@ -3,9 +3,13 @@ import{Movie}from"./movie"
 export class Imdb{
     public peliculas:Movie[]
     constructor(peliculas:Movie[]){
-        this.peliculas=peliculas
+        this.peliculas=peliculas  
     }
     
-    }
+}
+
+    
+    
+    
     
 
